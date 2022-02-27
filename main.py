924eea4087ac7ca33ceb4f1ed5c2f648ec4ea1e7
renelync Cruz <62046975+renelyncruz@users.noreply.github.com>
2022-02-27 02:20:49 +0000

    Added dictionary variable, and first print statement.

---
+++ main.py
@@ -0,0 +1,10 @@
+dictionary = [
+  {
+    "Nouns": "Nouns are words that represent people, places, or things.",
+    "Adjectives": "Adjectives are words that describe people, places, or things.",
+    "Verbs": "Verbs are words that show an action(sing), occurence (develop), or state of being (exist).",
+    "Adverbs": "Adverbs are words that desribe verbs, usually ending in '-ly'. The also tell when, where, how often, and how much a verb is done."
+  }]
+
+print("Hello! Welcome to my MadLib program. Before we start, let's remember the definitions for nouns, adjectives, verbs, and adverbs.") 
+
