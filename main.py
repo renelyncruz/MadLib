9288fb3854f26ff880ff911552d61fcd92cf5c55
renelync Cruz <62046975+renelyncruz@users.noreply.github.com>
2022-02-27 03:13:18 +0000

    Added in story_parts list to inquire for specific words from the user; added in a dictionary to store the user's words; added in a loop asking the user for the words, and place those words inside my empty dictionary.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#dictionary to remind user of specific definitions
 dictionary = [
   {
     "Nouns": "Nouns are words that represent people, places, or things.",
@@ -7,9 +8,34 @@ dictionary = [
   }]
 
 print("Hello! Welcome to my MadLib program. Before we start, let's remember the definitions for nouns, adjectives, verbs, and adverbs.\n") 
-print(dictionary[0]["Nouns"])
-print(dictionary[0]["Adjectives"])
-print(dictionary[0]["Verbs"])
-print(dictionary[0]["Adverbs"])
-print("\nAll righty then! Now that we understand those words, let's make a story together! Please provide me with some information below:")
+#print statements calling each key
+print(dictionary["Nouns"])
+print(dictionary["Adjectives"])
+print(dictionary["Verbs"])
+print(dictionary["Adverbs"])
+print("\nAll righty then! Now that we understand those words, let's make a story together! You'll need to provide me with some information below.\n")
+#list to inquire for words from user
+story_parts = [
+  "1st adjective ",
+  "1st noun ",
+  "adverb, ending in -ly ",
+  "2nd adjective ",
+  "3rd adjective ",
+  "your favorite food ",
+  "4th adjective ",
+  "2nd noun ",
+  "1st verb, ending in -ed ",
+  "3rd noun ",
+  "2nd verb, ending in -ed ",
+  "5th adjective "
+]
+#dictionary to hold the users words
+bucket_for_story_parts = {}
+#loop that fills story parts list
+for part in story_parts:
+  #asks the user for different word types
+  word = input(f"Please enter {part}:")
+  #places each chosen word inside the dictionary. It turns each word type into a key, while the user's word choice is that key's value.
+  bucket_for_story_parts[part] = word
 
+print(bucket_for_story_parts)
