42dfa846d1aca973eb33a4373f57686093cee9d2
renelync Cruz <62046975+renelyncruz@users.noreply.github.com>
2022-02-27 02:32:14 +0000

    printed dictionary, added second print statement

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ dictionary = [
     "Adverbs": "Adverbs are words that desribe verbs, usually ending in '-ly'. The also tell when, where, how often, and how much a verb is done."
   }]
 
-print("Hello! Welcome to my MadLib program. Before we start, let's remember the definitions for nouns, adjectives, verbs, and adverbs.") 
+print("Hello! Welcome to my MadLib program. Before we start, let's remember the definitions for nouns, adjectives, verbs, and adverbs.\n") 
+print(dictionary[0]["Nouns"])
+print(dictionary[0]["Adjectives"])
+print(dictionary[0]["Verbs"])
+print(dictionary[0]["Adverbs"])
+print("\nAll righty then! Now that we understand those words, let's make a story together! Please provide me with some information below:")
 
